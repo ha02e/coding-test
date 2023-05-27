0103caacbcefc48debc49fd7b79b646d65e0ce2a
ha02e <121777501+ha02e@users.noreply.github.com>
2023-05-27 15:53:00 +0900

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -3,10 +3,11 @@ class Solution {
         int answer = 0;
         
         String num_s=Integer.toString(num);
+        char k_s=(char)(k+'0');
         
         char[] num_arr=num_s.toCharArray();
         for(int i=0; i<num_arr.length; i++){
-            if(num_arr[i]==Character.forDigit(k,10)){
+            if(num_arr[i]==k_s){
                 answer=i+1;
                 break;
             }else{
