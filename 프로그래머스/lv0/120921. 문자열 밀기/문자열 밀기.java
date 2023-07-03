8bb762545eb3d6cc028c03aa2d381cc83a080777
ha02e <ha02e@naver.com>
2023-07-03 23:35:00 +0900

    [level 0] Title: 문자열 밀기, Time: 1.28 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String A, String B) {
+        int answer = 0;
+        
+        String copy=A;
+        for(int i=0; i<A.length(); i++){
+            if(copy.equals(B)){
+                return answer;
+            }
+            String str=copy.substring(copy.length()-1);
+            copy=str+copy.substring(0,copy.length()-1);
+            answer+=1;
+        }
+        
+        return -1;
+    }
+}
