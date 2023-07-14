5c16a1d63177926978e4723e578419b753b67c06
ha02e <ha02e@naver.com>
2023-07-14 23:27:12 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int num=x;
+        int sum=0;
+        
+        while(num!=0){
+            sum += num%10;
+            num /= 10;
+        }
+        
+        answer=x%sum==0?true:false;
+        return answer;
+    }
+}
